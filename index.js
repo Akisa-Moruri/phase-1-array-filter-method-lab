536e130966a539f812781d238a0af6877305e0c8
Akisa-Moruri <akisamoruri@gmail.com>
2024-03-25 23:00:52 +0300

    array-filter index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+// findMatching - returns drivers whose names match the provided string, case insensitive
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+// fuzzyMatch - returns drivers whose names begin with the provided letters
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+
+// matchName - returns drivers whose names match the provided string argument
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+}
